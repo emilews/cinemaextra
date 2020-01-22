ae99b9427c5d1f535c6a7bc1f157ff0f24f4f991
Isaac Bustamante <izaack.97@gmail.com>
2020-01-22 15:48:22 -0700

    Nos quedamos en el paso 15 de la practica

--- private/initialize.php
+++ private/initialize.php
@@ -8,6 +8,18 @@
   define("PUBLIC_PATH", PROJECT_PATH . '/public');
   define("SHARED_PATH", PRIVATE_PATH . '/shared');
 
+  /**
+   * Asigna el URL raiz a una constante de PHP
+   * * Este valor se puede ser asignado en el codigo:
+   * define("WWW_ROOT",'/globe_bank/public/');
+   * * o puede ser definido dinamicamente obteniendo 
+   * el URL y  buscando en el la carpeta "/public" 
+  */
+
+  $public_end = strpos($_SERVER['SCRIPT_NAME'], '/public') + 7;
+  $doc_root = substr($_SERVER['SCRIPT_NAME'], 0, $public_end);
+  define("WWW_ROOT", $doc_root);
+
   require_once('functions.php');
 
 ?>
